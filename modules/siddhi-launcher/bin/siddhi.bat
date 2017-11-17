68bc2c363bc11301036add94b17062e6284ef705
Raveen Savinda Rathnayake <raveen.s.r@gmail.com>
2017-11-17 12:58:24 +0530

    Updating bat file

--- modules/siddhi-launcher/bin/siddhi.bat
+++ modules/siddhi-launcher/bin/siddhi.bat
@@ -80,7 +80,6 @@ if ""%1""==""--debug""  goto commandDebug
 shift
 goto setupArgs
 
-
 rem ----- commandDebug ---------------------------------------------------------
 :commandDebug
 shift
@@ -100,24 +99,19 @@ if "%OS%"=="Windows_NT" @setlocal
 if "%OS%"=="WINNT" @setlocal
 goto runServer
 
-
 rem ----------------- Execute The Requested Command ----------------------------
-
 :runServer
 
 set CMD=%*
 
 rem ---------- Add jars to classpath ----------------
 
-set SIDDHI_CLASSPATH="%SIDDHI_HOME%\lib\*.jar";%SIDDHI_CLASSPATH%
-
 set JAVA_ENDORSED="%JAVA_HOME%\jre\lib\endorsed";"%JAVA_HOME%\lib\endorsed"
 
-set CMD_LINE_ARGS=-Xbootclasspath/a:%SIDDHI_XBOOTCLASSPATH% -Xms256m -Xmx1024m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath="%SIDDHI_HOME%\heap-dump.hprof" -Dcom.sun.management.jmxremote %JAVA_OPTS% -Djava.endorsed.dirs=%JAVA_ENDORSED%  -Dsiddhi.home="%SIDDHI_HOME%" -classpath %SIDDHI_CLASSPATH% -Djava.command="%JAVA_HOME%\bin\java" -Djava.opts="%JAVA_OPTS%" -Djava.io.tmpdir="%SIDDHI_HOME%\tmp" -Denable.nonblocking=false -Dfile.encoding=UTF8 -Dlog4j.configuration="%SIDDHI_HOME%\conf\log4j.properties"
+set CMD_LINE_ARGS=-Xbootclasspath/a:%SIDDHI_XBOOTCLASSPATH% -Xms256m -Xmx1024m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath="%SIDDHI_HOME%\heap-dump.hprof" -Dcom.sun.management.jmxremote %JAVA_OPTS% -Djava.endorsed.dirs=%JAVA_ENDORSED% -Dsiddhi.home="%SIDDHI_HOME%" -classpath %SIDDHI_CLASSPATH% -Djava.command="%JAVA_HOME%\bin\java" -Djava.opts="%JAVA_OPTS%" -Djava.io.tmpdir="%SIDDHI_HOME%\tmp" -Denable.nonblocking=false -Dfile.encoding=UTF8 -Dlog4j.configuration=file:"%SIDDHI_HOME%\conf\log4j.properties"
 
 :runJava
 "%JAVA_HOME%\bin\java" %CMD_LINE_ARGS% org.wso2.siddhi.sdk.launcher.Main %CMD%
-if "%ERRORLEVEL%"=="121" goto runJava
 :end
 goto endlocal
 
